589f52615474df38db6949e337f526942e30e073
Madushan D. Memmendarachchi <damiththa@gmail.com>
2020-01-03 15:22:28 -0500

    getting gamercard and displaying in slack friendly format

--- slack_slash-commands/handler.py
+++ slack_slash-commands/handler.py
@@ -12,20 +12,52 @@ XBOX_API_KEY = os.environ['XBOX_API_KEY']
 
 def get_xboxgamer_info(event, context):
 
+    print (event)
+    # TODO: should do a auth code check
+
     # url
     url = XBOX_API_BASE_URL + XBOX_PROFILE_ID
 
-    r = get_gamercard(url) # Getting my gamercard from XBOXAPI
-    print (r.status_code)
-    print (r.content)
+    res = get_gamercard(url) # Getting my gamercard from XBOXAPI
+    print (res.status_code)
+    print (res.content)
 
     # TODO: Need to better handle non-200 response
-    if r.status_code != 200:
+    if res.status_code != 200:
         print ('something went wrong in api return')
+    
+    xboxapi_return = json.loads(res.content) # getting json in as python data
+    # print (xboxapi_return)
+
+    # print (xboxapi_return['gamertag']) # gamertag
 
     # NOTEME: This is the message reply message body
     body = {
-        "returnBody": "Come back to this message"
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Hello " + str(xboxapi_return['name']) + " here is your gamercard."
+                }
+            },
+            {
+			    "type": "divider"
+		    },
+            {
+                "type": "section",
+                "block_id": "section567",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "\n *gamertag: *" +  str(xboxapi_return['gamertag']) + "\n *motto: *" + "_"+str(xboxapi_return['motto'])+"_" + "\n *gamerscore: *" + str(xboxapi_return['gamerscore'])
+                },
+                "accessory": {
+                    "type": "image",
+                    "image_url": "https://image-ssl.xboxlive.com/global/t.fffe07d1/tile/0/21046",
+                    "alt_text": str(xboxapi_return['gamertag']) + " gamer pic."
+                }
+            }
+        ]
     }
 
     response = {
